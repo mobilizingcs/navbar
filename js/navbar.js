665169afb6751541a949dc1ae0ed4943158ad9da
Jeroen <jeroenooms@gmail.com>
2014-08-28 20:42:42 +0200

    escape from double iframe

--- js/navbar.js
+++ js/navbar.js
@@ -1,61 +1,61 @@
-$(function(){
-
+if (window != window.top) {
     //escape from iframe in iframe
-    if (window != window.top) {
-        window.location.replace("home.html");
-        return;
-    }
-    
-    //initiate the client
-    var oh = Ohmage("/app", "mobilize-navbar");
-    var iframe = $("#meta_iframe");    
+    window.location.replace("home.html");
+} else {
+    //load the actual page
+    $(function(){
 
-    //hide on page load
-    $("#username-li").hide();
-    $("#logoutlink").hide();
-    $("#login-li").hide();
-    $("#help").hide();
+        //initiate the client
+        var oh = Ohmage("/app", "mobilize-navbar");
+        var iframe = $("#meta_iframe");    
 
-    //logout link
-    $("#logoutlink").click(function(e){
-        e.preventDefault();
-        oh.logout().always(function(){
-            window.location.reload()
-        })
+        //hide on page load
+        $("#username-li").hide();
+        $("#logoutlink").hide();
+        $("#login-li").hide();
+        $("#help").hide();
+
+        //logout link
+        $("#logoutlink").click(function(e){
+            e.preventDefault();
+            oh.logout().always(function(){
+                window.location.reload()
+            })
+        });
+
+        //watch iframe changes
+        iframe.on("load", function(){
+            //update the browser address bar
+            location.hash = btoa(iframe[0].contentWindow.location.href);
+
+            //check if user has logged in or out
+            updateUserInfo();
+        });
+
+        //populate navbar with username
+        function updateUserInfo(){
+            //retrieve user info
+            oh.user.whoami().done(function(name){
+                $("#username").text(name);
+                $("#user-modal-username").text(name);
+                $("#username-li").show();
+                $("#login-li").hide();
+                $("#logoutlink").show();
+
+                oh.user.read({user_list : name}).done(function(r){
+                    $("#user-modal-firstname").text(r[name]["first_name"] || "unknown");
+                    $("#user-modal-lastname").text(r[name]["last_name"] || "unknown");
+                    $("#user-modal-org").text(r[name]["organization"] || "unknown");
+                    $("#user-modal-email").text(r[name]["email_address"] || "unknown");
+                });
+            }).error(function(){
+                $("#login-li").show();
+            }).always(function(){
+                $("#help").show();
+            });        
+        }
     });
-
-    //watch iframe changes
-    iframe.on("load", function(){
-        //update the browser address bar
-        location.hash = btoa(iframe[0].contentWindow.location.href);
-
-        //check if user has logged in or out
-        updateUserInfo();
-    });
-
-    //populate navbar with username
-    function updateUserInfo(){
-        //retrieve user info
-        oh.user.whoami().done(function(name){
-            $("#username").text(name);
-            $("#user-modal-username").text(name);
-            $("#username-li").show();
-            $("#login-li").hide();
-            $("#logoutlink").show();
-
-            oh.user.read({user_list : name}).done(function(r){
-                $("#user-modal-firstname").text(r[name]["first_name"] || "unknown");
-                $("#user-modal-lastname").text(r[name]["last_name"] || "unknown");
-                $("#user-modal-org").text(r[name]["organization"] || "unknown");
-                $("#user-modal-email").text(r[name]["email_address"] || "unknown");
-            });
-        }).error(function(){
-            $("#login-li").show();
-        }).always(function(){
-            $("#help").show();
-        });        
-    }
-});
+}
 
 
 /*
