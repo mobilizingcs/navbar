59c00b56b7c6702e93bf69b0231b0f5f71a7affb
Steve Nolen <technolengy@gmail.com>
2015-07-17 10:56:28 -0700

    add changepassword to user dropdown

--- views/navbar.js
+++ views/navbar.js
@@ -7,8 +7,10 @@ define([
   'oh',
   'config',
   'text!templates/navbar.html',
-  'text!templates/accountdetails.html'
-], function($, _, Backbone, bootstrap, vent, oh, config, navbarTemplate, accountDetailsTemplate){
+  'text!templates/accountdetails.html',
+  'text!templates/changepassword.html',
+  'text!templates/message.html'
+], function($, _, Backbone, bootstrap, vent, oh, config, navbarTemplate, accountDetailsTemplate, changePasswordTemplate, messageTemplate){
   var navbarView = Backbone.View.extend({
     el: $("#navbar"),
     initialize: function(){
@@ -18,7 +20,9 @@ define([
       vent.on('ohmage:error:auth', this.logged_out);
     },
     events: {
-      "click #logoutlink": "logout"
+      "click #logoutlink": "logout",
+      "click #change-password-modal-link": "changePasswordModal",
+      "submit #change-password-form": "changePassword"
     },
     logout: function(){
       oh.logout().done(function(){
@@ -42,13 +46,55 @@ define([
         oh.user.read({user_list : username}).done(function(r){
           var user_details = r[username];
           user_details['permissions']['admin'] ? $("#admin-link").show() : $("#admin-link").hide();
-          that.accountDetails(username, user_details);
+          that.accountDetailsModal(username, user_details);
+          //that.changePasswordModal(username);
         });
       })
     },
-    accountDetails: function(username, details){
+    accountDetailsModal: function(username, details){
       var template = _.template(accountDetailsTemplate);
       $("#navbar").append(template({username: username, user:details}));
+    },
+    changePasswordModal: function(){
+      var template = _.template(changePasswordTemplate);
+      $("#navbar").append(template({username: $("#nav-username").text()}));
+      $("#change-password-form").validate({
+        rules: {
+          "change_password_new_password": {
+            required: true,
+            minlength: 8
+          },
+          "change_password_new_password_confirm": {
+            required: true,
+            minlength: 8,
+            equalTo: "#change_password_new_password"
+          }
+        }
+      })
+    },
+    changePassword: function(e){
+      e.preventDefault();
+      var that = this;
+      if (!$('#change-password-form').valid()) {
+          return false;
+      }
+      oh.user.change_password({
+        user: $("#change-password-username").val(),
+        password: $("#change-password-current-password").val(),
+        new_password: $("#change_password_new_password").val()
+      }).done(function(){
+        $("#change-password-current-password").val('');
+        $("#change_password_new_password").val('');
+        $("#change_password_new_password_confirm").val('');
+        that.changePasswordMessage("Successfully changed password", 'success')
+      }).error(function(msg){
+        that.changePasswordMessage(msg, 'danger')
+      });
+    },
+    changePasswordMessage: function(msg, status){
+      var target = '.errordiv.change'; 
+      var template = _.template(messageTemplate);
+      $(target).html(template({status: status, msg: msg})).fadeIn(100);
     }
   });
 
