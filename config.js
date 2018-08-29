d2531d01f610bd61fd2d82cfe1e8ec75c29df7e1
Shuhao Wu <sw3023@columbia.edu>
2018-08-29 14:03:12 -0700

    Update config.js

--- config.js
+++ config.js
@@ -9,18 +9,21 @@ define([
   'config/mobilize',
   'config/mobilizelabs',
   'config/mobilize-in-a-box',
-  'config/ids'
-], function($, _, Backbone, vent, oh, oldohmageConfig, ohmageConfig, mobilizeConfig, mobilizelabsConfig, mobilizeInABoxConfig, idsConfig){
+  'config/ids',
+  'config/idscentinela'
+], function($, _, Backbone, vent, oh, oldohmageConfig, ohmageConfig, mobilizeConfig, mobilizelabsConfig, mobilizeInABoxConfig, idsConfig, idscentinelaConfig){
   //run on init to determine what sort of ohmage deploy this is.
   //this should really be 'application_name' param from config/read
   //but some sort of require/depends issue with require.js is happening.
   var config = {}
   switch (location.hostname) {
     case 'ids.mobilizingcs.org':
-    case 'ids.centinela.k12.ca.us':
     case 'tools.idsucla.org':
     case 'mobilize.lausd.org':
       config = idsConfig;
+      break;
+    case 'ids.centinela.k12.ca.us':
+      config = idscentinelaConfig;
       break;
     case 'lausd.mobilizingcs.org':
     case 'test.mobilizingcs.org':
