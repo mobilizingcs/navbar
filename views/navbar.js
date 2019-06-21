ce6111eba81d4599870dd79dade9070d392c21eb
sw3023 <sw3023@columbia.edu>
2019-06-21 12:25:53 -0700

    Update navbar.js

--- views/navbar.js
+++ views/navbar.js
@@ -70,7 +70,7 @@ define([
           user_details['permissions']['admin'] ? $("#admin-link").show() : $("#admin-link").hide();
           user_details['permissions']['can_create_classes'] ? $("#admin-link2").show() : $("#admin-link2").hide();
 		  user_details['permissions']['can_create_classes'] ? $("#admin-link3").show() : $("#admin-link3").hide();
-		  user_details['permissions']['can_create_classes'] ? $("a#admin-link77").attr("href", "#shiny?admin=true") : $("a#admin-link77").attr("href", "#shiny?username=idle");
+		  user_details['permissions']['can_create_classes'] ? $("a#admin-link77").attr("href", "#shiny?admin=true&username="+username) : $("a#admin-link77").attr("href", "#shiny?&username="+username);
           that.accountDetailsModal(username, user_details);
         });
       })
