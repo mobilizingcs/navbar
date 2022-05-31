08b166163e730bbbc4faf865f626e4851fa528a3
shuhao <sw3023@columbia.edu>
2022-05-31 11:45:06 -0700

    add idsportalConfig

--- config.js
+++ config.js
@@ -11,13 +11,20 @@ define([
   'config/mobilize-in-a-box',
   'config/ids',
   'config/idscentinela',
-  'config/idslongbeach'
-], function($, _, Backbone, vent, oh, oldohmageConfig, ohmageConfig, mobilizeConfig, mobilizelabsConfig, mobilizeInABoxConfig, idsConfig, idscentinelaConfig,idslongbeachConfig){
+  'config/idslongbeach',
+  'config/idsportal'
+], function($, _, Backbone, vent, oh, oldohmageConfig, ohmageConfig, mobilizeConfig, mobilizelabsConfig, mobilizeInABoxConfig, idsConfig, idscentinelaConfig,idslongbeachConfig,idsportalConfig){
   //run on init to determine what sort of ohmage deploy this is.
   //this should really be 'application_name' param from config/read
   //but some sort of require/depends issue with require.js is happening.
   var config = {}
   switch (location.hostname) {
+    case 'portal.idsucla.org':
+    case 'portal.mobilizingcs.org':
+    case 'portal2.idsucla.org':
+    case 'portal2.mobilizingcs.org':
+      config = idsportalConfig;
+      break;
     case 'ids.mobilizingcs.org':
     case 'ids.gseis.ucla.edu':
     case 'ids.stat.ucla.edu':
