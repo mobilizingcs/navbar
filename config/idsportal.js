5c58e8fbe7425d3a5a8164b7a66f2c4fa90c51ef
shuhao <sw3023@columbia.edu>
2022-06-01 20:13:24 -0700

    Update idsportal.js

--- config/idsportal.js
+++ config/idsportal.js
@@ -16,11 +16,12 @@ define([
   config.tools.add({"title":"Class Setup",  "href": "#classes",  "img": "assets/classsetup.png",  "description_html":"Class management tool"})
   config.tools.add({"title":"RStudio",  "href": "#rstudio", "img": "assets/rstudio.png",  "description_html":"User-friendly interface for R"})
   config.tools.findWhere({"title":"Wiki"}).set({"href": "https://curriculum.idsucla.org/video/", "img": "assets/help.png", "description_html":"Instructional Videos on Using the Tools"})
-  config.tools.findWhere({"title":"Wiki"}).remove()
+  config.tools.remove({"title":"Wiki","href": "https://curriculum.idsucla.org/video/", "img": "assets/help.png", "description_html":"Instructional Videos on Using the Tools"})
   config.apps.findWhere({"title":"Android"}).set({"img":"assets/ohmageX-android.png","href":"https://play.google.com/store/apps/details?id=edu.ucla.oit.idsucla&hl=en_US"})
   config.apps.findWhere({"title":"iOS"}).set({"img":"assets/ohmageX-ios.jpg","href":"https://itunes.apple.com/us/app/ids-ucla/id1422869521?mt=8"})
   config.navs.findWhere({"name":"Explore"}).set({"navs": [{"name": "Campaign Monitoring", "href": "#campaigns/#monitor"}, {"name": "Interactive Dashboard", "href":"#campaigns/#dashboard"}, {"name": "PlotApp", "href":"#campaigns/#plotapp"}, {"name":"RStudio", "href":"#rstudio"},{"name":"Demo Data", "href":"#demo"}]})
   config.navs.add({"name":"Classes", "short_name":"Classes", "href": "#classes"})
   config.tools.add({"title":"Help","href": "https://curriculum.idsucla.org/video/", "img": "assets/help.png", "description_html":"Instructional Videos on Using the Tools"})
+  window.config00=config
   return config;
 });
